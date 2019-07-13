c01c581e52235e5fe96560e5537d3e4aeecae9ce
nuttapo puttwong <59143413@g.cmru.ac.th>
2019-07-13 14:15:58 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) => res.send('ณัฐพล พุทธวงค์'))
+app.get('/', (req, res) => res.send('หน้าแรก'))
 app.get('/about', (req, res) => res.send('ณัฐพล'))
 app.get('/Contact', (req, res) => res.send('084-1395184'))
+app.get('/test', (req, res) => {
+    res.json({message :"test"})
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
